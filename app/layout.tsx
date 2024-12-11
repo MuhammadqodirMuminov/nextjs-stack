186d06b2a8c174bd4360138fa190558c7a3de128
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2024-12-11 14:07:29 +0500

    theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import ThemeProvider from '@/context/ThemeProvider';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Metadata } from 'next';
+import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -11,11 +13,34 @@ export const metadata: Metadata = {
 	},
 };
 
+export const inter = Inter({
+	subsets: ['latin'],
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	variable: '--font-inter',
+});
+
+export const spaceGrotest = Space_Grotesk({
+	subsets: ['latin'],
+	weight: ['300', '400', '500', '600', '700'],
+	variable: '--font-spaceGrotesk',
+	display: 'swap',
+	style: 'normal',
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<ClerkProvider>
+		<ClerkProvider
+			appearance={{
+				elements: {
+					formButtonPrimary: 'primary-gradient',
+					footerActionLink: 'primary-text-gradient hover:text-primary-500',
+				},
+			}}
+		>
 			<html lang='en'>
-				<body>{children}</body>
+				<body className={`${inter.variable} ${spaceGrotest.variable}`}>
+					<ThemeProvider>{children}</ThemeProvider>
+				</body>
 			</html>
 		</ClerkProvider>
 	);
